22455296307f6a27c814a09a3d02c0c052f55d09
gabrielabernice <gbernice@student.ciputra.ac.id>
2022-10-24 21:57:17 +0700

    Trial Commit

--- WardrobeChances/resources/views/about.blade.php
+++ WardrobeChances/resources/views/about.blade.php
@@ -7,7 +7,7 @@
     </div>
     <div class="text-center">
         <h1 class="mt-3">Our Logo</h1>
-        <p class="px-5">Our logo is made from the word "thrift", which is one of the activities we do to provide an opportunity to make used clothes it undergoes rework process, so that they can be used again.</p>
+        <p class="px-5">Our logo is made from the word <b>"thrift"</b>, which is one of the activities we do to provide an opportunity to make used clothes it undergoes rework process, so that they can be used again.</p>
         <h6 class="mb-3">#ChancesToWear</h6>
     </div>
 @endsection
